1fe32d4e6f35b35a9f03ae08911ff493cf65270d
Fabio Arnold <fabio@fabioarnold.de>
2020-11-28 13:21:38 +0100

    clean up geometry.zig

--- src/geometry.zig
+++ src/geometry.zig
@@ -3,11 +3,10 @@ pub fn Point(comptime T: type) type {
         x: T,
         y: T,
 
-        pub fn make(x: T, y: T) Point(T) {
-            return Point(T){
-                .x = x,
-                .y = y,
-            };
+        const Self = @This();
+
+        pub fn make(x: T, y: T) Self {
+            return .{ .x = x, .y = y };
         }
     };
 }
@@ -19,20 +18,17 @@ pub fn Rect(comptime T: type) type {
         w: T,
         h: T,
 
-        pub fn make(x: T, y: T, w: T, h: T) Rect(T) {
-            return Rect(T){
-                .x = x,
-                .y = y,
-                .w = w,
-                .h = h,
-            };
+        const Self = @This();
+
+        pub fn make(x: T, y: T, w: T, h: T) Self {
+            return .{ .x = x, .y = y, .w = w, .h = h };
         }
 
-        pub fn contains(self: Rect(T), point: Point(T)) bool {
+        pub fn contains(self: Self, point: Point(T)) bool {
             return point.x >= self.x and point.x < self.x + self.w and point.y >= self.y and point.y < self.y + self.h;
         }
 
-        pub fn overlaps(self: Rect(T), other: Rect(T)) bool {
+        pub fn overlaps(self: Self, other: Rect(T)) bool {
             return self.x < other.x + other.w and self.x + self.w > other.x and self.y < other.y + other.h and self.y + self.h > other.y;
         }
     };
